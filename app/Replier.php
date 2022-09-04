89168265bc32cdfbd91c8484c8ea5061d1efffed
kadir-erman <kadirerman@teknasyon.com>
2022-09-04 14:14:21 +0300

    Add a new class

---
+++ app/Replier.php
@@ -0,0 +1,69 @@
+<?php
+
+namespace App;
+
+use Illuminate\Http\JsonResponse;
+use Illuminate\Validation\ValidationException;
+
+class Replier
+{
+    /**
+     * @param bool $success
+     * @param int $code
+     * @param $data
+     * @param $message
+     * @return JsonResponse
+     */
+    public static function response(bool $success, int $code, $data = null, $message = null): JsonResponse
+    {
+        $body = [
+            'success' => $success,
+            'data' => $data,
+        ];
+
+        if (isset($message)) {
+            $body['message'] = $message;
+        }
+
+        return response()->json($body, $code);
+    }
+
+    /**
+     * @param $data
+     * @return JsonResponse
+     */
+    public static function responseSuccess($data): JsonResponse
+    {
+        return self::response(
+            true,
+            200,
+            $data,
+        );
+    }
+
+    /**
+     * @param $data
+     * @param $message
+     * @param int $code
+     * @return JsonResponse
+     */
+    public static function responseFalse($data = null, $message = null, int $code = 400): JsonResponse
+    {
+        return self::response(
+            false,
+            $code,
+            $data,
+            $message,
+        );
+    }
+
+    /**
+     * @throws ValidationException
+     */
+    public static function responseError($validator): JsonResponse
+    {
+        $response = self::responseFalse($validator->errors()->first(), 422);
+
+        throw new ValidationException($validator, $response);
+    }
+}
