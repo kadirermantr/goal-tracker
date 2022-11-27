e350ad243f6d9e77de46ed0dc94f0783ef4f1ec3
kadirerman <kadirermantr@gmail.com>
2022-11-27 23:54:26 +0300

    Revert "feat: add test user"
    This reverts commit ce13da986ac4c745ee9d16cb1cf5ebdc35a146b7.

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -14,12 +14,6 @@ class UserSeeder extends Seeder
      */
     public function run(): void
     {
-        User::create([
-            'name' => fake()->name(),
-            'email' => 'test@gmail.com',
-            'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
-        ]);
-
         User::factory(10)->create();
     }
 }
